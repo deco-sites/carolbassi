f941432d373ecf42635c961bdc1ebf1f7673978d
Ricardo Henrique Gonzaga <ricardo.gonzaga@agenciam3.com>
2023-07-18 03:24:30 -0300

    feat: add new validation for outofstock product page

--- components/product/OutOfStock.tsx
+++ components/product/OutOfStock.tsx
@@ -1,4 +1,5 @@
 import { useSignal } from "@preact/signals";
+import { useState } from "preact/compat";
 import { Runtime } from "deco-sites/fashion/runtime.ts";
 import type { Product } from "deco-sites/std/commerce/types.ts";
 import type { JSX } from "preact";
@@ -11,6 +12,8 @@ const notifyme = Runtime.create("deco-sites/std/actions/vtex/notifyme.ts");
 
 function Notify({ productID }: Props) {
   const loading = useSignal(false);
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const [showError, setShowError] = useState(false);
 
   const handleSubmit: JSX.GenericEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
@@ -23,7 +26,17 @@ function Notify({ productID }: Props) {
       const email =
         (e.currentTarget.elements.namedItem("email") as RadioNodeList)?.value;
 
+      if (!emailRegex.test(email)) {
+        setShowError(true);
+        return false;
+      }
+
+      if (!emailRegex) return false;
+
       await notifyme({ skuId: productID, name, email });
+      setShowError(false);
+    } catch {
+      setShowError(true);
     } finally {
       loading.value = false;
     }
@@ -40,14 +53,21 @@ function Notify({ productID }: Props) {
 
       <input
         placeholder="Nome"
-        class="h-[40px] border-b-[1px] border-solid border-primary"
+        class="h-[40px] border-b-[1px] border-solid border-primary outline-none focus-visible:border-b-[1px]"
         name="name"
       />
       <input
         placeholder="Email"
-        class="h-[40px] border-b-[1px] border-solid border-primary"
+        class="h-[40px] border-b-[1px] border-solid border-primary outline-none focus-visible:border-b-[1px]"
         name="email"
       />
+      {showError && (
+        <>
+          <h4 class="text-sm mt-2 top-full text-error">
+            E-mail inválido
+          </h4>
+        </>
+      )}
 
       <button
         class="btn disabled:loading bg-info text-primary normal-case rounded-none font-normal text-lg h-[59px]"
